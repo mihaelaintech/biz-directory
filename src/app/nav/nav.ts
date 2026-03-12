4ae8d9f14040c2b126efb0ca202d2559a3d13cf3
Mihaela <mihaelapetre140@gmail.com>
2026-03-12 19:00:30 +0000

    Fix Auth0 logout redirect

--- src/app/nav/nav.ts
+++ src/app/nav/nav.ts
@@ -10,9 +10,21 @@ import { AuthService } from '@auth0/auth0-angular';
   templateUrl: './nav.html',
   styleUrls: ['./nav.css']
 })
-
 export class NavComponent {
 
   constructor(public authService: AuthService) {}
+
+  logout() {
+    const returnTo =
+      window.location.hostname === 'mihaelaintech.github.io'
+        ? 'https://mihaelaintech.github.io/biz-directory/'
+        : window.location.origin;
+
+    this.authService.logout({
+      logoutParams: {
+        returnTo: returnTo
+      }
+    });
+  }
 
 }
